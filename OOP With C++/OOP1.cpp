ba3d121f3e095cd401f76a98f4cc5dc55fd1aeeb
AnsElwkel <anshesh039@gmail.com>
2024-06-18 19:57:50 +0300

    NT

---
+++ OOP With C++/OOP1.cpp
@@ -0,0 +1,8 @@
+class Rectangle{
+private:
+    double width , height;
+public:
+    Rectangle() Rectangle(0.0 , 0.0) :{}
+
+    Rectangle(double width , double height) : width(width) , height(height){}
+};
