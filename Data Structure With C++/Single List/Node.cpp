15b22e333af3e7b8d0941a043655c258621a365c
AnasElwkel <anshesh039@gmail.com>
2024-08-28 20:49:03 +0300

    Learn

--- Data Structure With C++/Single List/Node.cpp
+++ Data Structure With C++/Single List/Node.cpp
@@ -3,6 +3,7 @@ public:
     int data;
     Node *next;
 
+
     Node(int data) : data(data), next(nullptr) {
     }
 };
@@ -11,6 +12,13 @@ void printNodes(Node *node) {
     while (node != nullptr) {
         cout << node->data << " ";
         node = node->next;
+    }
+    cout << endl;
+}
+
+void printNodes1(Node *node) { // Using for loop
+    for(; node != nullptr; node = node->next) {
+        cout << node->data << " ";
     }
     cout << endl;
 }
@@ -29,7 +37,13 @@ void printNode_reverse(Node *node) {
     cout << node->data << ' ';
 }
 
-int main() {
+Node* find(int val , Node *node) {
+    for(; node != nullptr; node = node->next)
+        if(node->data == val) return node;
+    return nullptr;
+}
+
+void solve() {
     Node *node1 = new Node(0);
     Node *node2 = new Node(4);
     Node *node3 = new Node(10);
@@ -50,8 +64,11 @@ int main() {
     cout << node5->data << endl; // node5 23
 
     printNodes(node1);
+    printNodes1(node1);
     printNode2(node1);
     cout << endl;
     printNode_reverse(node1);
     cout << endl;
-}
+
+    cout << find(23, node1)->data << endl;
+    cout << find(14, node1)->data << endl; //Undefined behaviour
